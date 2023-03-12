57bd87d744f06100fcd9f63d3aa772f6c3e6588b
Revi1337 <david122123@gmail.com>
2023-03-13 00:23:38 +0900

    26. 컴포넌트 스캔 - 필터

---
+++ SpringCore1/SpringCore/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,32 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {}
+
+    @Test
+    public void filterScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean(BeanA.class);
+        assertThat(beanA).as("BeanA 타입의 Bean 이 Null 이 아니어야함").isNotNull();
+        assertThat(beanA).as("BeanA 타입의 Bean 은 BeanA 타입이어야함.").isInstanceOf(BeanA.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean(BeanB.class));
+    }
+
+}
---
+++ SpringCore1/SpringCore/src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,10 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
