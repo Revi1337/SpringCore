b576f73c9f79abe973f12068c5f5fdae2057d1f5
Revi1337 <david122123@gmail.com>
2023-03-13 23:05:13 +0900

    31. 의존관계 자동 주입 - Lombok 으로 대체

--- SpringCore1/SpringCore/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringCore1/SpringCore/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,21 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // 철저하게 DIP 를 지키고있음. (MemberRepository, DiscountPolicy 인터페이스에만 의존함. 구체적인 클래스에 전혀 의존하지 않음)
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired // MemberRepository 타입과 DiscountPolicy 타입의 Bean 을 의존관계를 자동으로 주입해줌. (생성자 주입)
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
