c6f6dfe4e26b71716b259260b314b3b59c055f3d
Revi1337 <david122123@gmail.com>
2023-03-13 23:55:53 +0900

    33. 의존관계 자동 주입 - 조회된 빈이 2개 이상- @Autowired

--- SpringCore1/SpringCore/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringCore1/SpringCore/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,14 +4,21 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component @RequiredArgsConstructor
+@Component
 public class OrderServiceImpl implements OrderService {
 
     // 철저하게 DIP 를 지키고있음. (MemberRepository, DiscountPolicy 인터페이스에만 의존함. 구체적인 클래스에 전혀 의존하지 않음)
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
