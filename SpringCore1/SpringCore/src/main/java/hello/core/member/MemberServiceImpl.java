0e6f2d9841c367869681af6d69556939a9cf083d
Revi1337 <david122123@gmail.com>
2023-03-13 18:11:49 +0900

    28. 의존관계 자동 주입 - 다양한 의존관계 주입 방법

--- SpringCore1/SpringCore/src/main/java/hello/core/member/MemberServiceImpl.java
+++ SpringCore1/SpringCore/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -9,11 +9,7 @@ public class MemberServiceImpl implements MemberService{
     // 철저하게 DIP 를 지키고있음. (MemberRepository 인터페이스에만 의존함. 구체적인 클래스에 전혀 의존하지 않음)
     private final MemberRepository memberRepository;
 
-    // MemberServiceImpl 입장에서 생성자를 통해 어떤 구현 객체가 들어올지(주입될지)는 알 수 없다 (생성자 주입패턴)
-    // MemberServiceImpl 의 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부( AppConfig )에서 결정된다.
-    // MemberServiceImpl 은 이제부터 의존관계에 대한 고민은 외부(AppConfig)에 맡기고 실행에만 집중하면 된다
-    // MemberServiceImpl 입장에서 보면 의존관계를 외부(AppConfig) 에서 주입해주는 것 같다고 해서, DI(Dependency Injection) 우리말로 의존관계 주입 또는 의존성 주입이라고 한다.
-    @Autowired // MemberRepository 타입의 Bean 을 의존관계를 자동으로 주입해줌.
+    @Autowired // MemberRepository 타입의 Bean 을 의존관계를 자동으로 주입해줌. (생성자 주입)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
