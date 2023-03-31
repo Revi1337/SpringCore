d0c7bceea46264e118db41402a08ed1d603cdb86
Revi1337 <david122123@gmail.com>
2023-03-31 14:42:58 +0900

    SpringCore2 Init

---
+++ SpringCore2/SpringCore2/src/main/java/com/example/springcore2/SpringCore2Application.java
@@ -0,0 +1,13 @@
+package com.example.springcore2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCore2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCore2Application.class, args);
+    }
+
+}
