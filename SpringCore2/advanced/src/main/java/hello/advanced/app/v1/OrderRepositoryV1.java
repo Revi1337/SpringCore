3527f379178aa9ecc23d1c628cff49aa2b838196
Revi1337 <david122123@gmail.com>
2023-08-15 11:49:02 +0900

    [SpringCore2 - 로그추적기 V1 - 적용]

---
+++ SpringCore2/advanced/src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,37 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository @RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 helloTraceV1;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = helloTraceV1.begin("OrderRepository.request()");
+
+            if (itemId.equals("ex"))
+                throw new IllegalStateException("예외 발생");
+            sleep(1000);
+
+            helloTraceV1.end(status);
+        } catch (Exception e) {
+            helloTraceV1.exception(status, e);
+            throw e;             // TODO 예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
