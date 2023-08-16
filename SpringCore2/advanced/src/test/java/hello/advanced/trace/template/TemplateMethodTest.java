ae9df34da7dfb568f5e97237d3dcd9031ffe588f
Revi1337 <david122123@gmail.com>
2023-08-16 10:44:02 +0900

    [SpringCore2 - TemplateMethodPattern 2]
    와 엄청나다.. 이게 디자인패턴..?
    
    * TemplateMethodPattern 은 다형성을 사용해서 변하는 부분과 변하지 않는 부분을 분리하는 방법

--- SpringCore2/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ SpringCore2/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,18 +1,15 @@
 package hello.advanced.trace.template;
 
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class TemplateMethodTest {
-
-    @Test
-    public void templateMethodV0() {
-        logic1();
-        logic2();
-    }
-
 
     public void logic1() {
         long startTime = System.currentTimeMillis();
@@ -33,4 +30,22 @@ public class TemplateMethodTest {
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
     }
+
+    @Test
+    @DisplayName(value = "Template Method 패턴 적용 전")
+    public void beforeApplyTemplateMethodPattern() {
+        logic1();
+        logic2();
+    }
+
+    @Test
+    @DisplayName(value = "Template Method 패턴 적용 후")
+    public void afterApplyTemplateMethodPattern() {
+        AbstractTemplate template1 =  new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 =  new SubClassLogic2();
+        template2.execute();
+    }
+
 }
