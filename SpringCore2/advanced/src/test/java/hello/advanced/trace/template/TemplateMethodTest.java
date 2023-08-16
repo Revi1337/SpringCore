98ef7217c84daa4c0d17e87b809f97682e5600c0
Revi1337 <david122123@gmail.com>
2023-08-16 10:56:42 +0900

    [SpringCore2 - TemplateMethodPattern 3]
    * TemplateMethodPattern 의 단점인 서브클래스를 계속만들어줘야한다는 것을 익명내부클래스로 해결할 수 있음.

--- SpringCore2/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ SpringCore2/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -39,12 +39,34 @@ public class TemplateMethodTest {
     }
 
     @Test
-    @DisplayName(value = "Template Method 패턴 적용 후")
+    @DisplayName(value = "Template Method 패턴 적용 후 --> 단점은 서브클래스를 계속 만들어내야한다는것 --> 익명 내부클래스를 사용하여 단점을 커버 가능하다.")
     public void afterApplyTemplateMethodPattern() {
         AbstractTemplate template1 =  new SubClassLogic1();
         template1.execute();
 
         AbstractTemplate template2 =  new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    @DisplayName(value = "Template Method 패턴 적용 후 --> 익명 내부클래스를 사용하여 서브클래스를 계속 만들어야하는 단점을 극복.")
+    public void afterApplyTemplateMethodPattern2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직 1 실행");
+            }
+        };
+        log.info("Class Name1 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직 2 실행");
+            }
+        };
+        log.info("Class Name2 = {}", template2.getClass());
         template2.execute();
     }
 
