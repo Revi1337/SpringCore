50c53e6ae44353f19d3441be5a1d1d72927188c6
Revi1337 <david122123@gmail.com>
2023-08-18 09:59:21 +0900

    [SpringCore2 - DecoratorPattern 예제 코드 1]

---
+++ SpringCore2/proxy-start/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,20 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    @DisplayName(value = "Decorator 패턴 사용 전")
+    public void noDecorator() {
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+
+        client.execute();
+    }
+}
