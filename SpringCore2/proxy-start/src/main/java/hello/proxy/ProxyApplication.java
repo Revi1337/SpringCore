c249c21ac2e562fc03bcbd3c79ce6863c3674c45
Revi1337 <david122123@gmail.com>
2023-08-20 14:51:10 +0900

    [SpringCore2 - 인터페이스 기반 프록시 적용]
    czx

--- SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,12 +2,13 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-
-@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
+//@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
