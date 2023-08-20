3005ff2010181f11c2d785f7425d14579e1120da
Revi1337 <david122123@gmail.com>
2023-08-20 15:57:34 +0900

    [SpringCore2 - 구체 클래스 기반 Proxy - 적용]

--- SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,12 +2,14 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
