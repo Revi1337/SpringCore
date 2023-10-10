d7ef76bd244fde8c48d350299f9b3d1a8400d372
Revi1337 <david122123@gmail.com>
2023-10-11 00:34:45 +0900

    [Spring Core2 - ProxyFactory 적용1]

--- SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,11 +6,13 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
+//@Import(DynamicProxyFilterConfig.class)
 // @Import(DynamicProxyBasicConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(InterfaceProxyConfig.class)
