53b047e1475f91ca34d6bdbcf17c582acbfd9e28
Revi1337 <david122123@gmail.com>
2023-10-11 17:39:06 +0900

    [Spring Core2 - BeanPostProcessor 적용]

--- SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore2/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,11 +8,13 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
+//@Import(ProxyFactoryConfigV2.class)
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(DynamicProxyFilterConfig.class)
 // @Import(DynamicProxyBasicConfig.class)
