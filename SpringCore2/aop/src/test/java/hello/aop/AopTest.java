7634704b5d19b3458314d8007e465f41af28fbed
Revi1337 <david122123@gmail.com>
2023-10-13 08:37:38 +0900

    [Spring Core2 - 스프링 AOP 구현1 - 시작]

--- SpringCore2/aop/src/test/java/hello/aop/AopTest.java
+++ SpringCore2/aop/src/test/java/hello/aop/AopTest.java
@@ -2,6 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,11 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     private final OrderService orderService;
