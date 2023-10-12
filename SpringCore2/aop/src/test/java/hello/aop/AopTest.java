d0edaa2d9d6b0011042ae2cd253f856789eed002
Revi1337 <david122123@gmail.com>
2023-10-13 08:26:04 +0900

    [Spring Core2 - 스프링 AOP 구현 - 예제 프로젝트 만들기]

---
+++ SpringCore2/aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,49 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    private final OrderService orderService;
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public AopTest(OrderService orderService, OrderRepository orderRepository) {
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+    @Test
+    @DisplayName("AOP 가 적용되었나 테스트")
+    public void aopInfo() {
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    public void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    public void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("예외 발생!");
+
+        assertThatIllegalStateException().isThrownBy(() -> orderService.orderItem("ex"))
+                .withMessage("예외 발생!");
+    }
+}
